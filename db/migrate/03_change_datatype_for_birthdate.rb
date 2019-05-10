f30fd1070094111101d41467832768655c0c5453
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 01:37:37 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
-    change_column(:students, :, type)
+    change_column(:students, :birthday, :datetime)
   end
 
 end
